ca320b97861b9f052ad870b867b634d8262e4db5
Ayesha Ilyas <annie16950@gmail.com>
2025-02-08 22:58:16 -0500

    patch cors problem

--- backend/app.py
+++ backend/app.py
@@ -1,12 +1,15 @@
 from flask import Flask
 import os
-from flask import Flask, flash, request, redirect, url_for
+from flask import Flask, request
 from werkzeug.utils import secure_filename
+from flask_cors import CORS
 
-UPLOAD_FOLDER = './uploads'
+UPLOAD_FOLDER = 'uploads'
 ALLOWED_EXTENSIONS = {'pdf'}
 
 app = Flask(__name__)
+CORS(app)  # Enable CORS for all routes
+
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 def allowed_file(filename):
@@ -17,20 +20,18 @@ def allowed_file(filename):
 def upload_file():
     if request.method == 'POST':
         # check if the post request has the file part
+        print(request.files)
         if 'file' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
+            return "file not found"
         file = request.files['file']
         # If the user does not select a file, the browser submits an
         # empty file without a filename.
         if file.filename == '':
-            flash('No selected file')
-            return redirect(request.url)
+            return "not file provided"
         if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            print(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return redirect(url_for('download_file', name=filename))
+            filename = os.path.join(app.config['UPLOAD_FOLDER'], secure_filename(file.filename))
+            file.save(filename)
+            return "saved file"
     return '''
     <!doctype html>
     <title>Upload new File</title>
